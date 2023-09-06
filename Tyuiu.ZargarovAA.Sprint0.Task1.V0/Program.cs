af8849dcb72c269ed708f60a23d97aada2375f89
Tyuiu.ZargarovAA@outlook.com <fairmarko@gmail.com>
2023-09-06 13:19:12 +0500

    добавил Task1

--- Tyuiu.ZargarovAA.Sprint0.Task1.V0/Program.cs
+++ Tyuiu.ZargarovAA.Sprint0.Task1.V0/Program.cs
@@ -13,7 +13,7 @@ namespace Tyuiu.ZargarovAA.Sprint0.Task1.V0
             Console.WriteLine("Hello, World");
             string name = "Айхан";
             int age = 18;
-            Console.WriteLine("Здравствуйте, меня зовут " + name + " мне " + age + " лет!");
+            Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
             Console.ReadKey();
         }
     }
